53d60a9e179dab2c5dd339dabce05121fd9d3b90
khansum <sumsuddin.khan@capita.co.uk>
2018-10-25 12:01:53 +0100

    github commit

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,12 +1,18 @@
 import numpy as np
 
 def get_intercept(X, y):
+    '''
+    Calculating the function intercept
+    :param X:
+    :param y:
+    :return:
+    '''
     X_offset = np.average(X, axis=0)
     _X = X - X_offset
     y_offset = np.average(y, axis=0)
     _y = y - y_offset
 
-    # X, y, X_offset, y_offset, X_scale
+    # X, y, X_offset, y_offset, X_scale ##
     coef_, _residues, rank_, singular_ = np.linalg.lstsq(_X, _y)
 
     #Calculating the intercept:
